64f53be4d1d29d511eca57bcd5f00f88c2828dc1
gouravbose27 <gourav.bose27@gmail.com>
2026-09-23 21:23:23 +0200

    Enhance welcome message in main.py with ASCII art for Moose Programming Language

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from repl.repl import repl
 
 if __name__ == "__main__":
-    print("\nWelcome to the Moose Programming Language!\n")
-    print("Starting Moose REPL...")
+    print("\nWelcome to the Moose Programming Language!")
+    print('''
+_____          _____  
+\_   \        /  __/
+ _\   \      /  /__
+ \___  \____/   __/
+     \_       _/
+       | @ @  \_
+     _/     /
+    /o)  (o/\ \_
+    \_____/ /
+      \____/
+    ''')
+
+    print("Starting Moose REPL...\n")
     repl()
